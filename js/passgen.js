c6d3b73c8d6c02763f480b21e99388ca6e4561f3
Dan Piazza <DanielJPiazza@users.noreply.github.com>
2020-10-24 20:12:09 -0400

    Add checkboxes for choosing character types in generated password.

--- js/passgen.js
+++ js/passgen.js
@@ -1,9 +1,11 @@
 "use strict";
 
-document.getElementById("passwordResult").value = "";
-
 var maxPassLength = 64;
-var defaultPassLength = 14;
+var defaultPassLength = 18;
+var uppercaseFlag = false;
+var lowercaseFlag = true;
+var numbersFlag = false;
+var symbolsFlag = false;
 
 var slider = document.getElementById("passwordLengthSlider");
 slider.value = defaultPassLength;
@@ -12,8 +14,16 @@ var passwordOutput = document.getElementById("sliderVal");
 passwordOutput.innerHTML = slider.value;
 
 function passGen(length) {
+    document.getElementById("confirmCopy").innerHTML = "";
+    
     var result = "";
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+    var characters = "";
+    
+    if (uppercaseFlag) { characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; }
+    if (lowercaseFlag) { characters += "abcdefghijklmnopqrstuvwxyz"; }
+    if (numbersFlag) { characters += "0123456789"; }
+    if (symbolsFlag) { characters += "!@#$%^&*()"; }
+    
     var charactersLength = characters.length;
     for (var i = 0; i < length; i++) {
        result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -22,10 +32,25 @@ function passGen(length) {
     return result;
 }
 
+var checkboxes = document.getElementsByTagName('input');
+for (var i = 0; i < checkboxes.length; i++)  {
+    if (checkboxes[i].type == 'checkbox')   {
+        checkboxes[i].checked = false;
+    }
+}
+
+document.getElementById("passwordResult").value = "";
+document.getElementById('lowercaseFlag').checked = lowercaseFlag
+
 slider.oninput = function() {
     slider.value = this.value;
     passwordOutput.innerHTML = slider.value;
 }
+
+document.getElementById("uppercaseFlag").onchange = function() { uppercaseFlag = !uppercaseFlag }
+document.getElementById("lowercaseFlag").onchange = function() { lowercaseFlag = !lowercaseFlag }
+document.getElementById("numbersFlag").onchange = function() { numbersFlag = !numbersFlag }
+document.getElementById("symbolsFlag").onchange = function() { symbolsFlag = !symbolsFlag }
 
 document.getElementById("generatePasswordButton").addEventListener("click", function() {
         document.getElementById("passwordResult").value = passGen(slider.value);
